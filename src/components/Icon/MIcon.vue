0ea07fcaaabba2a2c3f9099fc79a40f43bb25409
Conrado Cañas <conrado.canas@m0.xyz>
2025-06-02 00:05:09 -0300

    release: v0.1.13 with new icon system and tokens subfolder

---
+++ src/components/Icon/MIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  size: {
+    type: [String, Number],
+    default: 24
+  },
+  color: {
+    type: String,
+    default: 'currentColor'
+  }
+})
+</script>
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    :fill="color"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <slot />
+  </svg>
+</template> 
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { App } from 'vue'
 import MHeader from './components/MHeader.vue'
 import MFooter from './components/MFooter.vue'
+import MIcon from './components/Icon/MIcon.vue'
+import MIconCopy from './components/Icon/MIconCopy.vue'
+import MIconTokenM from './components/Icon/tokens/MIconTokenM.vue'
 
 // Export individual components
-export { MHeader, MFooter }
+export { MHeader, MFooter, MIcon, MIconCopy, MIconTokenM }
 
 // Export as Vue plugin
 export default {
   install(app: App) {
     app.component('MHeader', MHeader)
     app.component('MFooter', MFooter)
+    app.component('MIcon', MIcon)
+    app.component('MIconCopy', MIconCopy)
+    app.component('MIconTokenM', MIconTokenM)
   },
   MHeader,
-  MFooter
+  MFooter,
+  MIcon,
+  MIconCopy,
+  MIconTokenM
 } 
