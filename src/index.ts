9e35711194f65de46694877b9aae3feb34152699
Conrado Cañas <conrado.canas@m0.xyz>
2025-06-02 00:13:29 -0300

    fix: export MIconTokenWM in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import MFooter from './components/MFooter.vue'
 import MIcon from './components/Icon/MIcon.vue'
 import MIconCopy from './components/Icon/MIconCopy.vue'
 import MIconTokenM from './components/Icon/tokens/MIconTokenM.vue'
+import MIconTokenWM from './components/Icon/tokens/MIconTokenWM.vue'
 
 // Export individual components
-export { MHeader, MFooter, MIcon, MIconCopy, MIconTokenM }
+export { MHeader, MFooter, MIcon, MIconCopy, MIconTokenM, MIconTokenWM }
 
 // Export as Vue plugin
 export default {
@@ -16,10 +17,12 @@ export default {
     app.component('MIcon', MIcon)
     app.component('MIconCopy', MIconCopy)
     app.component('MIconTokenM', MIconTokenM)
+    app.component('MIconTokenWM', MIconTokenWM)
   },
   MHeader,
   MFooter,
   MIcon,
   MIconCopy,
-  MIconTokenM
+  MIconTokenM,
+  MIconTokenWM
 } 
